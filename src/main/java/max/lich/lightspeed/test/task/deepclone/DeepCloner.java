74c5b32c380a9d5d7489ecd090f616ddab0ee95a
MaksimShulakov <MSHulakov_ad@inno.tech>
2025-02-16 17:05:55 +0400

    added empty class DeepCloner

---
+++ src/main/java/max/lich/lightspeed/test/task/deepclone/DeepCloner.java
@@ -0,0 +1,12 @@
+package max.lich.lightspeed.test.task.deepclone;
+
+import java.lang.reflect.Field;
+
+public class DeepCloner {
+    public <T> T deepClone(T t) {
+
+
+        return null;
+    }
+
+}
