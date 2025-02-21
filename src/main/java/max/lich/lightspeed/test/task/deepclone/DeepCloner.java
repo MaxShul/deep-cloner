ba766f06c290e5c74e4eb88dedf9ba3ef2ba51d2
MaksimShulakov <MSHulakov_ad@inno.tech>
2025-02-21 13:49:17 +0400

    add check if it's not a basic immutable class to DeepCloner.deepClone()

--- src/main/java/max/lich/lightspeed/test/task/deepclone/DeepCloner.java
+++ src/main/java/max/lich/lightspeed/test/task/deepclone/DeepCloner.java
@@ -2,21 +2,26 @@ package max.lich.lightspeed.test.task.deepclone;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Map;
 
 public class DeepCloner {
-    public <T> T deepClone(T t) {
-        Class<?> aClass = t.getClass();
-        return createClone(t, aClass);
-    }
-
-    private <T> T createClone(T t, Class<?> aClass) {
+    public <T> T deepClone(T objectToClone) {
+        Class<?> aClass = objectToClone.getClass();
         Constructor<?> defaultConstructor = ReflectionUtils.getDefaultConstructor(aClass);
         T newInstance = ReflectionUtils.createNewInstance(defaultConstructor);
 
         Field[] fields = aClass.getDeclaredFields();
         for (Field field : fields) {
             field.setAccessible(true);
-            Object fieldValue = ReflectionUtils.getFieldValue(t, field);
+            Object fieldValue = ReflectionUtils.getFieldValue(objectToClone, field);
+            if (ReflectionUtils.notBasicImmutableType(fieldValue)) {
+                if (fieldValue.getClass().isArray() || fieldValue instanceof Collection<?> || fieldValue instanceof Map<?,?>) {
+                    //todo
+                } else {
+                    fieldValue = deepClone(fieldValue);
+                }
+            }
             ReflectionUtils.setFieldValue(field, newInstance, fieldValue);
         }
 
